d1637e32812d60483f48d23b36ccf8fd9866851c
Simon Hall <simon970@gmail.com>
2020-02-22 23:00:59 +1300

    add make blue function

--- game.js
+++ game.js
@@ -3,43 +3,47 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+function start() {
+    bindEventListeners(document.getElementsByClassName('board')[0].children)
 }
 
-function bindEventListeners (dots) {
-  for (var i = 0; i < dots.length; i++) {
-    // BIND YOUR EVENT LISTENERS HERE
-    // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-  }
+function bindEventListeners(dots) {
+    for (var i = 0; i < dots.length; i++) {
+        // BIND YOUR EVENT LISTENERS HERE
+        // The first one is provided for you
+        dots[i].addEventListener('contextmenu', makeGreen)
+    }
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+    evt.preventDefault()
+    evt.target.classList.toggle('green')
+    updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue(evt) {
+    evt.preventDefault()
+    evt.target.classList.toggle('blue')
+    updateCounts()
+}
 // CREATE FUNCTION hide HERE
 
-function updateCounts () {
-  var totals = {
-    blue: 0,
-    green: 0,
-    invisible: 0
-  }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+function updateCounts() {
+    var totals = {
+        blue: 0,
+        green: 0,
+        invisible: 0
+    }
 
-  // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+    // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
+    // Once you've done the counting, this function will update the display
+    displayTotals(totals)
 }
 
-function displayTotals (totals) {
-  for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
-  }
+function displayTotals(totals) {
+    for (var key in totals) {
+        document.getElementById(key + '-total').innerHTML = totals[key]
+    }
 }
